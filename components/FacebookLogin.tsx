cdf9c53d2a4655063f072f7667656e4eb0d9f35d
Joe Hsu <jhsu.x1@gmail.com>
2020-11-18 15:32:48 -0500

    add login add export task

---
+++ components/FacebookLogin.tsx
@@ -0,0 +1,58 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import * as React from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useAtom } from "jotai";
+
+import { userAtom } from "../store";
+
+export const FacebookLogin = () => {
+  const [errors, setErrors] = useState(null);
+  const [user, setUser] = useAtom(userAtom);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, [setUser]);
+
+  const provider = useMemo(() => {
+    const fb = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().useDeviceLanguage();
+    return fb;
+  }, []);
+
+  const onLogin = useCallback(() => {
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(() => {
+        return firebase.auth().signInWithRedirect(provider);
+      });
+  }, [provider]);
+  const onLogout = useCallback(() => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setUser(null);
+      });
+  }, [setUser]);
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          {user.displayName}
+          <button onClick={onLogout}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={onLogin}>Login with Facebook</button>
+      )}
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import { Locations } from "../components/Locations";
 import { useFirestore } from "../components/firebase";
 import { useFirebaseLocations } from "../hooks/firebase";
 
+import { FacebookLogin } from "../components/FacebookLogin";
+
 export default function IndexPage() {
   const db = useFirestore();
   useFirebaseLocations({ db });
 
   return (
     <div>
+      <FacebookLogin />
       <Locations />
     </div>
   );
