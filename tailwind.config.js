d4f293d659eb20f31f55805d95d19c74b6b9a624
Joe Hsu <jhsu.x1@gmail.com>
2020-11-19 11:40:21 -0500

    add purge templates for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  purge: ["./components/**/*.tsx", "./pages/**/*.tsx", "./hooks/*.ts"],
   theme: {
     extend: {
       colors: {},
       fontFamily: {
         sans: ["Roboto", "sans-serif"],
-        heading: ['"Open Sans"', "Roboto", "sans-serif"]
-      }
-    }
-  }
+        heading: ['"Open Sans"', "Roboto", "sans-serif"],
+      },
+    },
+  },
 };
