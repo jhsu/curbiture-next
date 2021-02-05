b10d85e3e0c40f3817027299c2fd2ebb2163db02
Joe Hsu <jhsu.x1@gmail.com>
2021-02-05 00:41:37 -0500

    add next-on-netlify for server functions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
+  target: "experimental-serverless-trace",
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
