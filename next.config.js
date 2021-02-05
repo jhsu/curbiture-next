021260b88cee62b5066103029364e6a5afe6ed9a
Joe Hsu <jhsu.x1@gmail.com>
2021-02-05 03:21:18 -0500

    remove target

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  target: "experimental-serverless-trace",
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
